a8f0350bdf898a559b30bc348f45bbfa22b0a160
john king <johnking2010@me.com>
2018-07-24 18:01:38 +0100

    multiple articles rendered to screen, via Article[] - array of type article

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit, HostBinding } from '@angular/core';
+import { 
+  Component, 
+  OnInit, 
+  Input,
+  HostBinding } from '@angular/core';
 
 import { Article } from './article.model'; 
 
@@ -10,24 +14,17 @@ import { Article } from './article.model';
 export class ArticleComponent implements OnInit {
   // the CSS class we want to apply to the 'host' of this component
   @HostBinding('attr.class') cssClass='row'; 
-  // instance of the 'Article model'
-  article: Article;
+  // 
+  @Input() article: Article;
 
   constructor() { 
-    // Using an instance of the 'Article model' to get independent data:
-    this.article = new Article(
-      'Angular 6.0.9',
-      'http://angular.io',
-      10);
+    // empty, as article is populated via the Input now
   }
 
   voteUp() {
     // increment votes... 
-    //[this way is BAD PRACTICE, component is directly modifying the Article's internal properties directly!]
-    /*this.article.votes += 1;*/
-
+    /*GOOD practice - called via the Model's own method */  
     this.article.voteUp();
-
     return false
     // *** ABOVE line added to PREVENT the browser from PROPAGATING events upwards!
     // achieved via returning a boolean value of FALSE from the click event handler !!!
