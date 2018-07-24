65caa36bd072012e0752ab750f585360359c1a95
john king <johnking2010@me.com>
2018-07-24 17:19:49 +0100

    Article Model implemented, UI reading data from the instance passed to it - ng book 2 p49

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, HostBinding } from '@angular/core';
 
+import { Article } from './article.model'; 
+
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
@@ -8,22 +10,19 @@ import { Component, OnInit, HostBinding } from '@angular/core';
 export class ArticleComponent implements OnInit {
   // the CSS class we want to apply to the 'host' of this component
   @HostBinding('attr.class') cssClass='row'; 
-  // sum of all upvaotes (minus downvotes)
-  votes: number;
-  // article title:
-  title: string;
-  // article link:
-  link: string;
+  // instance of the 'Article model'
+  article: Article;
 
   constructor() { 
-    // setting some default attributes
-    this.title = 'Angular 6.0.9 (angular-cli 6.0.8)';
-    this.link = 'http://angular.io';
-    this.votes = 10;
+    // Using an instance of the 'Article model' to get independent data:
+    this.article = new Article(
+      'Angular 6.0.9',
+      'http://angular.io',
+      10);
   }
 
   voteUp() {
-    this.votes += 1;
+    this.article.votes += 1;
 
     return false
     // *** ABOVE line added to PREVENT the browser from PROPAGATING events upwards!
@@ -31,7 +30,7 @@ export class ArticleComponent implements OnInit {
   }
 
   voteDown() {
-    this.votes -= 1;
+    this.article.votes -= 1;
 
     return false
   } 
